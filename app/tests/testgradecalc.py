79a9b2b3c3ee5ea89b1729b071211a38d0e04cd9
CandiseS <sneed469@gmail.com>
2021-10-02 16:24:36 -0500

    test

--- app/tests/testgradecalc.py
+++ app/tests/testgradecalc.py
@@ -1,2 +1,15 @@
 # Use unittest library to create a unit test method to test the calculate_avg_grade function created in the gradecalc module
 # Make sure you include multiple unit tests to test that the function acts as expected in different scenarios
+import unittest
+from app.src.Student import Student
+from app.src.gradecalc import calc_avg_grade
+class GradeCaltest(unittest.TestCase):
+    def test_cal_grade(self):
+        input = [
+            Student('candise', 20, 1824, 80), Student('candise', 20, 1824, 100), Student('emily', 25, 1850, 90)
+        ]
+
+        expected = {1824: 90, 1850: 90}
+
+        actual = calc_avg_grade(input)
+        self.assertEquals(expected, actual)
--- app/tests/testutils.py
+++ app/tests/testutils.py
@@ -1,2 +1,13 @@
 # Create a unit test method to test the calculate_avg function created in the utils module
 # Make sure you include multiple unit tests to test that the function acts as expected in different scenarios
+
+import unittest
+from app.src.utils import calc_avg
+
+class Utiltest(unittest.TestCase):
+
+     def test_cal_avg(self) :
+        numbers = [5, 5, 5, 5]
+        expected_num = 5
+        actual_num = calc_avg(numbers)
+        self.assertEquals(expected_num, actual_num)
